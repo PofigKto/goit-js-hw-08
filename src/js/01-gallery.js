84b9c7ea8eaaec8e84b1a9339494348ef95d5d5e
Protopoop Aleksandr <alexandr2723@gmail.com>
2022-09-27 22:36:39 +0300

    !

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,4 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items.js';
